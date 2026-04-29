400556947d2133210626447a89f4765850690ded
flori-taishi <frnetotea@gmail.com>
2026-04-29 16:38:54 +0300

    Vanzare_UI

--- UI_VANZARE.py
+++ UI_VANZARE.py
@@ -1,4 +1,24 @@
-"""MODUL: VÂNZARE (Point of Sale)
-Scop: Înregistrarea comenzilor și a plăților (Cash/Card).
-Cerința: 2.a) Înregistrarea comenzilor.
-"""
+import flet as ft
+from LOGICA_DATABASE.models import Produs, Angajat
+from FUNCTII_SQL import inregistreaza_vanzare
+
+def VanzarePage(page: ft.Page):
+    produse = Produs.objects.all()
+    angajat = Angajat.objects.first()
+
+    def finalizeaza(e):
+        if not dropdown.value: return
+        inregistreaza_vanzare(angajat.id, [{'id': int(dropdown.value), 'cantitate': 1}], "Cash")
+        page.snack_bar = ft.SnackBar(ft.Text("Vânzare reușită în Oracle!"))
+        page.snack_bar.open = True
+        page.update()
+
+    dropdown = ft.Dropdown(
+        label="Alege Produs",
+        options=[ft.dropdown.Option(key=str(p.id), text=p.nume_produs) for p in produse]
+    )
+
+    return ft.Column([
+        ft.Text("Terminal Vânzare", size=25, weight="bold"),
+        dropdown,
+ft.ElevatedButton("Finalizează Tranzacție", on_click=finalizeaza, icon="check")    ])
